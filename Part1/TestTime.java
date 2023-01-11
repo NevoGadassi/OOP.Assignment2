5ed9bf8f552a0614652d19dd9fa363ae0492cb49
NevoGadassi <118170075+NevoGadassi@users.noreply.github.com>
2023-01-11 16:08:50 +0200

    Add files via upload

---
+++ Part1/TestTime.java
@@ -0,0 +1,30 @@
+
+
+    public class TestTime {
+        public static void main(String[] args)  {
+            long start,end,finaltime ;
+            int lines;
+
+            String[] filesname = Ex2_1.createTextFiles( 1500, 2, 10000);
+            start = System.currentTimeMillis();
+            lines = Ex2_1.getNumOfLines(filesname);
+            end = System.currentTimeMillis();
+            finaltime=end-start;
+            System.out.println("Calculation time using function 2  -  lines: "+ lines +" time: "+finaltime+"ms");
+
+            start = System.currentTimeMillis();
+            lines = Ex2_1.getNumOfLinesThreads(filesname);
+            end = System.currentTimeMillis();
+            finaltime=end-start;
+            System.out.println("Calculation time using function 3 -  lines: "+ lines +" time: "+finaltime+"ms");
+
+            start = System.currentTimeMillis();
+            lines = Ex2_1.getNumOfLinesThreadPool(filesname);
+            end = System.currentTimeMillis();
+            finaltime=end-start;
+            System.out.println("Calculation time using function 4-  lines: "+ lines +" time: "+finaltime+"ms");
+
+
+        }
+    }
+
